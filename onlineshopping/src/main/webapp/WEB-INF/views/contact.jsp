10dbd14e0caabe7f58a11fbecbfd45ab4e0a4cc7
saket <sktmakhija@gmail.com>
2020-04-04 12:36:17 +0530

    worked on css and theme

---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<h1>contact Us</h1>
+		<hr/>
+		<p>this is contact page</p>
+	</div>
+</div>
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,22 @@
+//solving active menu problem
+
+$(function(){
+	
+	switch(menu) {
+	
+	case 'About':
+		$('#about').addClass('active');
+		break;
+		
+	case 'Contact':
+		$('#contact').addClass('active');
+		break;
+		
+	default:
+		$('#home').addClass('active');
+		break;
+	}
+	
+	
+	
+});
